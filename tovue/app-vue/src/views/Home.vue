e55f17fd2fc2c370f6f8b42d5f9a21e6dc990558
Lars Næsbye Christensen <lars@naesbye.dk>
2021-10-28 23:13:04 +0200

    Try full replace to get the damn map

--- tovue/app-vue/src/views/Home.vue
+++ tovue/app-vue/src/views/Home.vue
@@ -16,72 +16,72 @@
 </template>
 
 <script>
-import { pageMeta } from '@/MetaData'
-import { getAssetLink, loadImage } from '@/HelperFunctions'
+import Hero from '@/components/home/Hero'
+import Filters from '@/components/shared/Filters'
 import Infoboxes from '@/components/home/Infoboxes'
-// import MapContainer from '@/components/map/MapContainer'
-
+import Results from '@/components/results/Results'
+import HeaderBackground from '@/components/shared/HeaderBackground'
+import MapContainer from '@/components/map/MapContainer'
+import { computed, provide } from 'vue'
+import { useStore } from 'vuex'
 export default {
-  name: 'Home',
-  components: {
-    Hero: () => import('@/components/shared/Hero'),
-    UiHorizontalScroller: () => import('@/components/shared/baseUi/UiHorizontalScroller'),
-    LinkBox: () => import('@/components/home/LinkBox'),
-    ExtLinkBox: () => import('@/components/home/ExtLinkBox'),
-    MapContainer: () => import('@/components/map/MapContainer')
-  },
+  name: 'Apps',
+  components: { Infoboxes, MapContainer, Hero, Filters, Results, HeaderBackground },
+
   data () {
     return {
-      coverImageUrl: '',
-      showMap: true,
+      arketyper: [],
+      infoboxes: [],
+      showMap: false,
+      tag: []
     }
   },
-  computed: {
-    pageAssets () {
-      return this.$store.state.HomeAssets.data
-    },
-    // title () {
-    //   return pageMeta.forside.title
-    // },
-    // summary () {
-    //   return pageMeta.forside.summary
-    // },
+  setup () {
+    const store = useStore()
+    provide('providedFilters', computed(() => store.state.Filters.data))
   },
-  methods: {},
-  mounted () {
-    this.$nextTick(() => {
-      const linkBoxAssetsIds = [1725, 1719, 1717]
-      const coverImageId = pageMeta.forside.coverId
-      const ids = [coverImageId, ...linkBoxAssetsIds]
+  methods: {
+    toggleMap () {
+      this.showMap = !this.showMap
+    }
+  },
+  created () {
+    this.$store.dispatch('InfoBoxes/clear')
+    this.$store.dispatch('Arketyper/clear')
 
-      this.$store.dispatch('HomeAssets/get', ids).then(() => {
-        linkBoxAssetsIds.forEach((id, i) => {
-          const url = getAssetLink(id, this.pageAssets)
-          loadImage(url).then(() => {
-            this.linkBoxes[i].imageUrl = url
-          })
-        })
-        const coverUrl = getAssetLink(coverImageId, this.pageAssets)
-        loadImage(coverUrl).then(() => {
-          this.coverImageUrl = coverUrl
-        })
-      })
+    this.$store.dispatch('Arketyper/get').then(() => {
+      this.arketyper = this.$store.state.Arketyper.data
     })
+
+    this.$store.dispatch('InfoBoxes/get').then(() => {
+      this.infoboxes = this.$store.state.InfoBoxes.data
+    })
+    window.dispatchEvent(new Event('resize', { bubbles: true, cancelable: false }))
+  },
+
+  computed: {
+    searchActive () {
+      return !!Object.keys(this.$route.query).length
+    },
+    updateFilters () {
+      return this.$store.state.Filters.data
+    },
+    mapgroups () {
+      const ret = []
+      const self = this
+      for (let i = 0, iEnd = self.infoboxes.length; i < iEnd; ++i) {
+        const arketypeID = self.infoboxes[i].arketypeID
+        const result = self.arketyper?.find(element => element === arketypeID)
+
+        if (result) {
+          ret[ret.length] = self.infoboxes[i]
+        }
+      }
+      return ret
+    }
   }
 }
 </script>
 
-<style lang="stylus" scoped>
-.hero-links
-  display flex
-  flex-wrap wrap
-
-.hero-links > a
-  color white
-  margin-right 1rem
-  white-space nowrap
-  font-weight bolder
-
-.bottom-ruler
-  border-bottom 2px solid var(--darkSteel)
+<style scoped>
 </style>
