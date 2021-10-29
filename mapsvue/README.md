ac593134fe19b73c8ce626d0c90554c1a7731578
Lars Næsbye Christensen <lars@naesbye.dk>
2021-10-29 11:00:10 +0200

    Revert "add the hkpn version"
    This reverts commit ef255d5092c05873ac6bd1dd84b531d19091f09c.

--- mapsvue/README.md
+++
@@ -1,2 +0,0 @@
-# Historiske kort 
-Denne applikation er udviklet med brug af API'et arkivmeta https://docs.kortforsyningen.dk/#arkivmeta
--- mapsvue/hkpn-vue/src/HelperFunctions.js
+++
@@ -1,9 +0,0 @@
-export const sendFilterData = (filterStates, store, router) => {
-  store.dispatch('Filters/setFilterData', filterStates).then((mutatedFilterStates) => {
-    store.dispatch('Kort/getAsParams', mutatedFilterStates).then((tempParams) => {
-      router.push({
-        query: tempParams
-      })
-    })
-  })
-}
