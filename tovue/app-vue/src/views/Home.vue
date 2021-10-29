e8ac279d7d39abbcaca44a64d477b5ecb5b544fb
Lars Næsbye Christensen <lars@naesbye.dk>
2021-10-29 10:35:50 +0200

    fix?

--- tovue/app-vue/src/views/Home.vue
+++ tovue/app-vue/src/views/Home.vue
@@ -1,4 +1,5 @@
 <template>
+  <p>Is this Home template here?</p>
   <Hero v-if="!searchActive" :key="1"/>
   <HeaderBackground v-else :key="2" />
   <section class="layout-2col_filters">
@@ -12,7 +13,6 @@
                :key="2"/>
       <MapContainer v-if="showMap" :key="4"/>
     </article>
-    <p>Is this Home template here?</p>
 
   </section>
 </template>
@@ -34,7 +34,7 @@ export default {
     return {
       arketyper: [],
       infoboxes: [],
-      showMap: false,
+      showMap: true,
       tag: []
     }
   },
