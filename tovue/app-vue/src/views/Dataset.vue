82ebc5a980803232d0f4134831d8296e7998faa7
Lars Næsbye Christensen <lars@naesbye.dk>
2021-11-12 23:22:09 +0100

    Revert "try without dataid"
    This reverts commit 64c15f38f4373c59b4034f0aa7fd367c0f327ada.

--- tovue/app-vue/src/views/Dataset.vue
+++ tovue/app-vue/src/views/Dataset.vue
@@ -23,10 +23,10 @@ export default {
     Map,
   },
   props: {
-    // dataId: {
-    //   type: String,
-    //   default: '-1'
-    // },
+    dataId: {
+      type: String,
+      default: '-1'
+    },
     tab: {
       type: String,
       default: 'data'
@@ -53,8 +53,7 @@ export default {
       return this.$store.state.DatasetMeta.status || 'loading'
     },
     id() {
-      return -1
-      // return Number(this.dataId) || -1
+      return Number(this.dataId) || -1
     },
     dataset() {
       return this.$store.state.DatasetMeta.data.id === this.id ? this.$store.state.DatasetMeta.data : null
