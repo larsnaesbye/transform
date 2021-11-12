81371dd4777b5d4a2fa450a943a20706e13d1ef2
Lars Næsbye Christensen <lars@naesbye.dk>
2021-11-12 21:09:20 +0100

    Revert "shoudl MS go?"
    This reverts commit 886af69f3af71cec7a6f753765fb44d3d858d181.

--- tovue/app-vue/src/views/Dataset.vue
+++ tovue/app-vue/src/views/Dataset.vue
@@ -7,6 +7,7 @@
         :datasetId="id"
         :data="mapData"
         :filters="filters.map"
+        :mapSettings="mapSettings"
         :columnDef="tableSettings.columnDef"
         :active=true
         @filters-updated="updateMapDataFilters"
