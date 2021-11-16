20bfbc3ba75a9a7712228d2e9fc756c16780d710
Lars Næsbye Christensen <lars@naesbye.dk>
2021-11-16 13:03:17 +0100

    Revert "Do without TileLayer?"
    This reverts commit 09aa717c2bb1813aa5b8704070d54919e6369050.

--- tovue/app-vue/src/components/dataset/MapFunctions.js
+++ tovue/app-vue/src/components/dataset/MapFunctions.js
@@ -34,3 +34,62 @@ const createUrl = function (service, auth) {
     return null
   }
 }
+
+export const createTileLayer = function (opt) {
+  // a lot of copy from okapi
+  const name = opt.name || ''
+  const type = opt.type || ''
+  const title = opt.title || name
+  const color = opt.color || 'black'
+  const visible = !!opt.visible
+  const auth = opt.auth
+  const service = opt.service || ''
+  const layer = opt.layer || ''
+  const style = opt.style || 'default'
+  const version = opt.version || '1.1.1'
+  const matrixSet = opt.matrixSet
+  const format = opt.format || ''
+  const size = opt.size || [256, 256]
+  const tileGrid = opt.tileGrid
+  const attributionText = opt.auth.source === 'kf' ? kfAttributionText : (opt.auth.source === 'df' ? dfAttributionText : opt.attribution)
+
+  let source = null
+  if (type === 'WMTS') {
+    source = new WMTS({
+      attributions: attributionText,
+      crossOrigin: 'Anonymous',
+      url: createUrl(service, auth),
+      layer: layer,
+      matrixSet: matrixSet,
+      format: format,
+      tileGrid: tileGrid,
+      style: style,
+      size: size
+    })
+  } else if (type === 'WMS') {
+    source = new TileWMS({
+      attributions: attributionText,
+      crossOrigin: 'Anonymous',
+      url: createUrl(service, auth),
+      params: {
+        LAYERS: layer,
+        VERSION: version,
+        TRANSPARENT: 'true',
+        FORMAT: format,
+        STYLES: style
+      }
+    })
+  } else {
+    return null
+  }
+  return new TileLayer({
+    opacity: 1.0,
+    title: title,
+    name: name,
+    color: color,
+    type: 'base',
+    visible: visible,
+    source: source,
+    preload: Infinity
+  })
+}
