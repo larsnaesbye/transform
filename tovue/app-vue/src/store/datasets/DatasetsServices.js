4fc1d057fbecd8228bfaa416b9916fc3cace0b53
Lars Næsbye Christensen <lars@naesbye.dk>
2021-11-18 15:35:36 +0100

    Revert "can't bother to parse"
    This reverts commit 2cbe522c572ce3acc0a618ee09372274767ac9ed.

--- tovue/app-vue/src/store/datasets/DatasetsServices.js
+++ tovue/app-vue/src/store/datasets/DatasetsServices.js
@@ -1,9 +1,10 @@
 import { ElasticSearch } from '@/store/shared/ElasticSearch'
+import { parseServices } from '@/store/shared/ParseData'
 import { getAll } from '@/store/shared/Queries'
 import { servicesSearch } from '@/store/shared/Paths'
 
 // eslint-disable-next-line one-var
-const crud = new ElasticSearch(getAll, servicesSearch),
+const crud = new ElasticSearch(getAll, servicesSearch, parseServices),
   state = crud.state,
   getters = crud.getters,
   actions = crud.actions,
