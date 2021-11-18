db2dc53894e6925e9fd6974982e3c72844118b7f
Lars Næsbye Christensen <lars@naesbye.dk>
2021-11-18 15:24:29 +0100

    don't expose the get

--- tovue/app-vue/src/store/shared/ElasticSearch.js
+++ tovue/app-vue/src/store/shared/ElasticSearch.js
@@ -1,4 +1,3 @@
-// copy paste fra Dataforsyningen
 import axios from 'axios'
 
 export class ElasticSearch {
@@ -12,21 +11,21 @@ export class ElasticSearch {
     }
 
     this.actions = {
-      get ({ commit, dispatch }, data) {
-        return new Promise((resolve, reject) => {
-          commit('request')
-          axios.post(window.location.origin + '/' + path, query(data), { timeout: 5000 })
-            .then((resp) => {
-              // console.log(path, resp)
-              commit('success', resp.data)
-              resolve(resp)
-            })
-            .catch((err) => {
-              commit('error', err)
-              reject(err)
-            })
-        })
-      },
+      // get ({ commit, dispatch }, data) {
+      //   return new Promise((resolve, reject) => {
+      //     commit('request')
+      //     axios.post(window.location.origin + '/' + path, query(data), { timeout: 5000 })
+      //       .then((resp) => {
+      //         // console.log(path, resp)
+      //         commit('success', resp.data)
+      //         resolve(resp)
+      //       })
+      //       .catch((err) => {
+      //         commit('error', err)
+      //         reject(err)
+      //       })
+      //   })
+      // },
       clear ({ commit, dispatch }) {
         commit('clear')
       }
