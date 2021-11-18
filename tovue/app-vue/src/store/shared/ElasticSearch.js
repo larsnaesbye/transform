ae6f05b129b1f69a5506ae1e40fa8f0fe7f3c2f6
Lars Næsbye Christensen <lars@naesbye.dk>
2021-11-18 15:28:16 +0100

    Revert "don't expose the get"
    This reverts commit db2dc53894e6925e9fd6974982e3c72844118b7f.

--- tovue/app-vue/src/store/shared/ElasticSearch.js
+++ tovue/app-vue/src/store/shared/ElasticSearch.js
@@ -1,3 +1,4 @@
+// copy paste fra Dataforsyningen
 import axios from 'axios'
 
 export class ElasticSearch {
@@ -11,21 +12,21 @@ export class ElasticSearch {
     }
 
     this.actions = {
-      // get ({ commit, dispatch }, data) {
-      //   return new Promise((resolve, reject) => {
-      //     commit('request')
-      //     axios.post(window.location.origin + '/' + path, query(data), { timeout: 5000 })
-      //       .then((resp) => {
-      //         // console.log(path, resp)
-      //         commit('success', resp.data)
-      //         resolve(resp)
-      //       })
-      //       .catch((err) => {
-      //         commit('error', err)
-      //         reject(err)
-      //       })
-      //   })
-      // },
+      get ({ commit, dispatch }, data) {
+        return new Promise((resolve, reject) => {
+          commit('request')
+          axios.post(window.location.origin + '/' + path, query(data), { timeout: 5000 })
+            .then((resp) => {
+              // console.log(path, resp)
+              commit('success', resp.data)
+              resolve(resp)
+            })
+            .catch((err) => {
+              commit('error', err)
+              reject(err)
+            })
+        })
+      },
       clear ({ commit, dispatch }) {
         commit('clear')
       }
