eaca96a9d8d423029ae7c40e796dcdb767d6c685
Lars Næsbye Christensen <lars@naesbye.dk>
2021-11-22 14:25:51 +0100

    datasetdata not needed p2

--- tovue/app-vue/src/store/index.js
+++ tovue/app-vue/src/store/index.js
@@ -3,9 +3,6 @@ import Vuex from 'vuex'
 
 import { Datasets } from '@/store/datasets/Datasets'
 import { DatasetsAssets } from '@/store/datasets/DatasetsAssets'
-// import { DatasetsAttributes } from '@/store/datasets/DatasetsAttributes'
-// import { DatasetsServices } from '@/store/datasets/DatasetsServices'
-// import { DatasetData } from '@/store/dataset/DatasetData'
 import { DatasetMeta } from '@/store/dataset/DatasetMeta'
 import { HomeAssets } from '@/store/home/HomeAssets'
 
@@ -15,9 +12,6 @@ export default new Vuex.Store({
   modules: {
     Datasets,
     DatasetsAssets,
-    // DatasetsAttributes,
-    // DatasetsServices,
-    // DatasetData,
     DatasetMeta,
     HomeAssets
   }
