035aaa4508786fe0ebdf16201435b12a34b5e516
Lars Næsbye Christensen <lars@naesbye.dk>
2021-11-22 15:56:25 +0100

    ok the datas then

--- tovue/app-vue/src/store/datasets/Datasets.js
+++ tovue/app-vue/src/store/datasets/Datasets.js
@@ -1,54 +1,34 @@
-import { ElasticSearch } from '@/store/shared/ElasticSearch'
-import { parseDatasets } from '@/store/shared/ParseData'
-import { productsSearch } from '@/store/shared/Paths'
+import {ElasticSearch} from '@/store/shared/ElasticSearch'
+import {parseDatasets} from '@/store/shared/ParseData'
+import {productsSearch} from '@/store/shared/Paths'
 
 // eslint-disable-next-line one-var
 const query = () => {
-    // eslint-disable-next-line multiline-ternary
-    return (process.env.NODE_ENV === 'production') ? {
-      size: 10000,
-      sort: [
-        { 'product_title.keyword': { order: 'desc' } }
-      ],
-      query: {
-        bool: {
-          must: [
-            {
-              match: {
-                tag_names: 'forsyningsdata'
-              }
-            }
-          ]
+        // eslint-disable-next-line multiline-ternary
+        return (process.env.NODE_ENV === 'production') ? {
+            size: 10000,
+            sort: [
+                {'product_title.keyword': {order: 'desc'}}
+            ],
+            query: {}
+        } : {
+            size: 10000,
+            sort: [
+                {'product_title.keyword': {order: 'desc'}}
+            ],
+            query: {}
         }
-      }
-    } : {
-      size: 10000,
-      sort: [
-        { 'product_title.keyword': { order: 'desc' } }
-      ],
-      query: {
-        bool: {
-          must: [
-            {
-              match: {
-                tag_names: 'forsyningsdata'
-              }
-            }
-          ]
-        }
-      }
-    }
-  },
-  crud = new ElasticSearch(query, productsSearch, parseDatasets),
-  state = crud.state,
-  getters = crud.getters,
-  actions = crud.actions,
-  mutations = crud.mutations
+    },
+    crud = new ElasticSearch(query, productsSearch, parseDatasets),
+    state = crud.state,
+    getters = crud.getters,
+    actions = crud.actions,
+    mutations = crud.mutations
 
 export const Datasets = {
-  namespaced: true,
-  state,
-  getters,
-  actions,
-  mutations
+    namespaced: true,
+    state,
+    getters,
+    actions,
+    mutations
 }
