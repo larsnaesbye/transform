9927df0485e5d7948b69828082a45cd5558efac0
Lars Næsbye Christensen <lars@naesbye.dk>
2021-11-22 15:44:07 +0100

    no homeassets, pt 2

--- tovue/app-vue/src/store/index.js
+++ tovue/app-vue/src/store/index.js
@@ -2,17 +2,13 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 
 import { Datasets } from '@/store/datasets/Datasets'
-// import { DatasetsAssets } from '@/store/datasets/DatasetsAssets'
 import { DatasetMeta } from '@/store/dataset/DatasetMeta'
-// import { HomeAssets } from '@/store/home/HomeAssets'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   modules: {
     Datasets,
-    // DatasetsAssets,
     DatasetMeta,
-    // HomeAssets
   }
 })
