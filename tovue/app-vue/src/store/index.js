e2c6f5e8a428bf8aa393a5ab0eae48c76ba5b7a5
Lars Næsbye Christensen <lars@naesbye.dk>
2021-11-22 15:07:37 +0100

    minus datasetmeta

--- tovue/app-vue/src/store/index.js
+++ tovue/app-vue/src/store/index.js
@@ -3,7 +3,7 @@ import Vuex from 'vuex'
 
 import { Datasets } from '@/store/datasets/Datasets'
 // import { DatasetsAssets } from '@/store/datasets/DatasetsAssets'
-import { DatasetMeta } from '@/store/dataset/DatasetMeta'
+// import { DatasetMeta } from '@/store/dataset/DatasetMeta'
 import { HomeAssets } from '@/store/home/HomeAssets'
 
 Vue.use(Vuex)
@@ -12,7 +12,7 @@ export default new Vuex.Store({
   modules: {
     Datasets,
     // DatasetsAssets,
-    DatasetMeta,
+    // DatasetMeta,
     HomeAssets
   }
 })
