c11a960953438eaa6a398e54e09b3167cbb551b1
Lars Næsbye Christensen <lars@naesbye.dk>
2021-11-23 10:08:08 +0100

    remove a lot more pt3

--- tovue/app-vue/src/views/Home.vue
+++ tovue/app-vue/src/views/Home.vue
@@ -4,7 +4,6 @@
         name="kort"
         :filters="filters.map"
         :mapSettings="mapSettings"
-        :columnDef="tableSettings.columnDef"
         :active="true"
         class="layout-1col"
     />
@@ -49,9 +48,6 @@ export default {
     summary() {
       return this.dataset ? this.dataset.summary : ''
     },
-    tableSettings() {
-      return this.dataset ? this.dataset.tableSettings : null
-    },
     fields() {
       return []
     },
@@ -72,7 +68,6 @@ export default {
   methods: {
     initDatasetData() {
       this.$store.dispatch('DatasetMeta/get', '2934')
-      // this.$store.dispatch('DatasetsServices/get')
     },
   }
 }
