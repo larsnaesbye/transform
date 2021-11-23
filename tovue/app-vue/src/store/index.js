7d0bf11a253dbc321934ca0d1a9574f4dd579280
Lars Næsbye Christensen <lars@naesbye.dk>
2021-11-23 13:14:38 +0100

    Revert "datasets too"
    This reverts commit 461dee1b215449070bdc11a47226c272220e7d3e.

--- tovue/app-vue/src/store/index.js
+++ tovue/app-vue/src/store/index.js
@@ -1,14 +1,14 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
-// import { Datasets } from '@/store/datasets/Datasets'
+import { Datasets } from '@/store/datasets/Datasets'
 // import { DatasetMeta } from '@/store/dataset/DatasetMeta'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   modules: {
-    // Datasets,
+    Datasets,
     // DatasetMeta,
   }
 })
