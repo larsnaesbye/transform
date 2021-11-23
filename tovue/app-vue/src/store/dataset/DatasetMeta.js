1250236933bd2234c0f3d77af77e2c56a2350fff
Lars Næsbye Christensen <lars@naesbye.dk>
2021-11-23 10:24:35 +0100

    Revert "remove a lot more pt2"
    This reverts commit 120bf46474882eed63f991fec429f514bf07b876.

--- tovue/app-vue/src/store/dataset/DatasetMeta.js
+++ tovue/app-vue/src/store/dataset/DatasetMeta.js
@@ -4,7 +4,7 @@ import {productsSearch} from '@/store/shared/Paths'
 import {getByIds} from '@/store/shared/Queries'
 
 const parse = (data) => {
-        return []
+        return parseDataset(data.hits.hits[0])
   },
   crud = new ElasticSearch(getByIds, productsSearch, parse),
   state = crud.state,
