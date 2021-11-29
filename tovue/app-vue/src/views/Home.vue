049404ed955d71a7ef88a0c63fca60bc7a6927b4
Lars Næsbye Christensen <lars@naesbye.dk>
2021-11-29 15:07:16 +0100

    Try hardcide

--- tovue/app-vue/src/views/Home.vue
+++ tovue/app-vue/src/views/Home.vue
@@ -39,7 +39,21 @@ export default {
   },
   computed: {
     dataset() {
-      return this.$store.state.DatasetMeta.data
+      // return this.$store.state.DatasetMeta.data
+      return {
+        mapConstants: {
+          center: [10.2457, 55.5147],
+          extent: [120000, 5900000, 1000000, 6500000],
+          resolutions: [1638.4, 819.2, 409.6, 204.8, 102.4, 51.2, 25.6, 12.8, 6.4, 3.2, 1.6, 0.8, 0.4, 0.2],
+          matrixIds: ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13']
+        },
+        clustering: true,
+        visualizationDef: {
+
+        },
+        filterDef: [
+        ]
+      }
     },
     services() {
       return []
