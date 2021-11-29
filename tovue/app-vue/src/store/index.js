203313763b343e767307ba3ea3f93cbb74e01de3
Lars Næsbye Christensen <lars@naesbye.dk>
2021-11-29 15:11:39 +0100

    Revert "datasetmeta"
    This reverts commit 3f9b7772171e965c2eb29e7db529c49e53f83e17.

--- tovue/app-vue/src/store/index.js
+++ tovue/app-vue/src/store/index.js
@@ -2,13 +2,13 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 
 import { Datasets } from '@/store/datasets/Datasets'
-// import { DatasetMeta } from '@/store/dataset/DatasetMeta'
+import { DatasetMeta } from '@/store/dataset/DatasetMeta'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   modules: {
     Datasets,
-    // DatasetMeta,
+    DatasetMeta,
   }
 })
