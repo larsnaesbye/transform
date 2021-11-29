3007b3e4c37c28dc8358d3746e45ff91e62b7460
Lars Næsbye Christensen <lars@naesbye.dk>
2021-11-29 13:21:19 +0100

    Clean

--- tovue/app-vue/vue.config.js
+++ tovue/app-vue/vue.config.js
@@ -139,12 +139,4 @@ module.exports = {
     }
 }
 
-/* function addStyleResource (rule) {
-  rule.use('style-resource')
-    .loader('style-resources-loader')
-    .options({
-      patterns: [
-        path.resolve(__dirname, './src/assets/css/fonts.styl')
-      ]
-    })
-} */
+
