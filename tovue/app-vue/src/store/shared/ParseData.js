e4a01984aa7be80e59d8a1123a699047add5af22
Lars Næsbye Christensen <lars@naesbye.dk>
2021-11-30 16:31:56 +0100

    Revert "tablesettings"
    This reverts commit bda81432569044ddf2975eb792777c40376ab694.

--- tovue/app-vue/src/store/shared/ParseData.js
+++ tovue/app-vue/src/store/shared/ParseData.js
@@ -27,6 +27,7 @@ export const parseDataset = (data) => {
       relTitles: source.product_rel_titles || '',
       relDataProducts: source.product_rel_dataproducts || [],
       fields: [],
+      tableSettings: datasetMeta[source.o_id].tableSettings || {},
       mapSettings: datasetMeta[source.o_id].mapSettings || {},
       dashboardSettings: datasetMeta[source.o_id].dashboardSettings || {},
       dataDescription: datasetMeta[source.o_id].dataInfo && datasetMeta[source.o_id].dataInfo.dataDescription ? datasetMeta[source.o_id].dataInfo.dataDescription : '',
@@ -56,6 +57,7 @@ export const parseDataset = (data) => {
       relTitles: '',
       relDataProducts: [],
       fields: [],
+      tableSettings: {},
       sectors: []
     }
 
