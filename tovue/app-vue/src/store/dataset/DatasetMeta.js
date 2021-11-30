8a0f2119b6c585c08e1e188397823988139f6d21
Lars Næsbye Christensen <lars@naesbye.dk>
2021-11-30 16:19:27 +0100

    trry not parsing

--- tovue/app-vue/src/store/dataset/DatasetMeta.js
+++ tovue/app-vue/src/store/dataset/DatasetMeta.js
@@ -3,7 +3,7 @@ import {parseDataset} from '@/store/shared/ParseData'
 import {getByIds} from '@/store/shared/Queries'
 
 const parse = (data) => {
-        return parseDataset(data.hits.hits[0])
+        return ''
   },
   crud = new ElasticSearch(getByIds, 'es/products/_search', parse),
   state = crud.state,
