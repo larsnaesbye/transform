3ef0cc9abde011a95a51a74e2b67031b59c0acc8
Lars Næsbye Christensen <lars@naesbye.dk>
2021-12-06 09:32:21 +0100

    clean up BaseInput

--- tovue/app-vue/src/components/shared/baseUi/BaseInput.vue
+++ tovue/app-vue/src/components/shared/baseUi/BaseInput.vue
@@ -2,12 +2,12 @@
   <div class="input">
     <label v-if="label">{{ label }}</label>
     <input
-      :value="modelValue"
-      v-bind="{
+        :value="modelValue"
+        v-bind="{
         ...$attrs,
         onChange: ($event) => { $emit('update:modelValue', $event.target.value) }
       }"
-      class="field">
+        class="field">
   </div>
 </template>
 
@@ -27,40 +27,39 @@ export default {
 </script>
 
 <style lang="stylus" scoped>
-  .input
-    display flex
-    flex-direction column
+.input
+  display flex
+  flex-direction column
 
-  label
-    color var(--darkSteel)
-    font-weight bold
-    font-size 0.625rem
-    line-height 0.75rem
-    letter-spacing 0.1em
-    text-transform uppercase
-    width 100%
+label
+  color var(--darkSteel)
+  font-weight bold
+  font-size 0.625rem
+  line-height 0.75rem
+  letter-spacing 0.1em
+  text-transform uppercase
+  width 100%
 
-  input
-    color var(--turkis)
-    border 0
-    width 100%
-    height 2rem
-    font-weight 300
-    font-size 0.875rem
-    padding 0
-    width 100%
+input
+  color var(--turkis)
+  border 0
+  width 100%
+  height 2rem
+  font-weight 300
+  font-size 0.875rem
+  padding 0
 
-  input:focus
-    outline none
+input:focus
+  outline none
 
-  /* Chrome, Safari, Edge, Opera */
-  input::-webkit-outer-spin-button,
-  input::-webkit-inner-spin-button
-    -webkit-appearance none
-    margin 0
+/* Chrome, Safari, Edge, Opera */
+input::-webkit-outer-spin-button,
+input::-webkit-inner-spin-button
+  -webkit-appearance none
+  margin 0
 
-  /* Firefox */
-  input[type=number]
-    -moz-appearance textfield
+/* Firefox */
+input[type=number]
+  -moz-appearance textfield
 
 </style>
