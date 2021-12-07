0a56b521a63ba76d8fded7413cf2fef708f41e6b
Lars Næsbye Christensen <lars@naesbye.dk>
2021-12-07 14:43:44 +0100

    Copy Home to Groenland

--- tovue/app-vue/src/views/Groenland.vue
+++ tovue/app-vue/src/views/Groenland.vue
@@ -1,172 +1,104 @@
 <template>
-  <main>
+  <main id="main" ref="main">
+    <Map
+        name="kort"
+        :filters="filters.map"
+        :mapSettings="mapSettings"
+        :active="true"
+        class="layout-1col"
+    />
   </main>
 </template>
 
 <script>
-import { pageMeta } from '@/MetaData'
+import Map from '@/components/dataset/Map'
 
 export default {
   components: {
+    Map,
   },
-  data () {
+  props: {
+    dataId: {
+      type: String,
+      default: '-1'
+    },
+    tab: {
+      type: String,
+      default: 'data'
+    }
+  },
+  data() {
     return {
-      filters: {},
-      filteredDatasets: []
+      filters: {
+        table: {},
+        map: {}
+      },
+      data: [],
     }
   },
   computed: {
-    datasetsStatus () {
-      return this.$store.state.Datasets.status
+    dataset() {
+      return this.$store.state.DatasetMeta.data
     },
-    pageTitle () {
-      return pageMeta.datasets.title
+    services() {
+      return []
     },
-    pageSummary () {
-      return pageMeta.datasets.summary
+    title() {
     },
-    datasets () {
-      return this.$store.state.Datasets.data
+    summary() {
+      return ''
     },
-    coverImageUrl () {
-      return this.getAssetLink(pageMeta.datasets.coverId)
+    tableSettings() {
+      return null
     },
-    datasetsCoverImageIds () {
-      const ids = [],
-        data = this.$store.state.Datasets.data
-      for (let i = 0; i < data.length; i++) {
-        ids[i] = data[i].image
-      }
-      return ids
+    fields() {
+      return []
     },
-    datasetsLogosIds () {
-      const ids = []
-      const data = this.$store.state.Datasets.data
-      for (let i = 0; i < data.length; i++) {
-        const lgs = data[i].logos
-        for (let j = 0; j < lgs.length; j++) {
-          ids.push(lgs[j].logoId)
-        }
-      }
-      return ids
+    mapSettings() {
+      return this.dataset.mapSettings
     },
-    pageAssets () {
-      const ids = []
-      const coverPhoto = pageMeta.datasets.coverId
-      if (coverPhoto) {
-        ids.push(coverPhoto)
-      }
-      return ids
-    },
-    allAssets () {
-      return [...this.datasetsCoverImageIds, ...this.datasetsLogosIds, ...this.pageAssets]
-    }
   },
-  methods: {
-    getAssetLink (id) {
-      const img = this.$store.state.DatasetsAssets.data.find((e) => {
-        return id === e.id
-      })
-      return img ? (window.location.origin + '/asset' + img.path + img.filename) : ''
-    },
-    setFilter (prop, filter) {
-      if (filter.type === 'multiselect') {
-        // setting filter initValue
-        const options = filter.options
-        const value = (!filter.initValue || filter.initValue === '_all') ? options : filter.initValue
+  created() {
+  },
+  mounted() {
 
-        // adding (or replacing) the filter
-        this.$set(prop, [filter.name], {
-          label: filter.label,
-          value: value,
-          options: options,
-          name: filter.name,
-          type: filter.type,
-          fieldId: filter.fieldId
-        })
-      } else if (filter.type === 'search') {
-        this.$set(prop, [filter.name], {
-          label: filter.label,
-          type: filter.type,
-          value: ''
-        })
-      }
-    },
-    resetAllFilters () {
-      const filters = [
-        {
-          label: 'Filtrer på søgeord',
-          name: 'search',
-          type: 'search',
-          fieldId: '',
-          initValue: ''
-        },
-        {
-          label: 'Filtrer på sektor',
-          name: 'sectors',
-          type: 'multiselect',
-          options: ['Distribution af el', 'Produktion af el', 'Drikkevand', 'Spildevand', 'Varme'],
-          fieldId: 'sectors',
-          initValue: '_all'
-        }
-      ]
-      filters.forEach((filter) => {
-        this.setFilter(this.filters, filter)
-      })
-    },
-    updateFilters (filters) {
-      this.filteredDatasets = this.filterData(this.datasets, this.filters)
-    },
-    filterData (rows, filters) {
-      for (const key in filters) {
-        const filter = filters[key]
-        if ((filter.type === 'multiselect') && (Array.isArray(filter.value)) && (filter.options.length > filter.value.length)) {
-          rows = rows.filter((row) => {
-            if (row[filter.fieldId] !== undefined) {
-              return filter.value.some((val) => {
-                if (Array.isArray(row[filter.fieldId])) {
-                  return row[filter.fieldId].some(item => item === val)
-                } else {
-                  return val === row[filter.fieldId]
-                }
-              })
-            } else {
-              return true
-            }
-          })
-        } else if (filter.type === 'search') {
-          const keys = ['fields', 'title', 'description']
-          rows = rows.filter((row) => {
-            return keys.some((key) => {
-              return String(row[key]).toLowerCase().indexOf(filter.value.toLowerCase()) > -1
-            })
-          })
-        }
-      }
-      return rows
-    }
   },
-  created () {
-    this.$store.dispatch('Datasets/get')
-      .then((response) => {
-        this.resetAllFilters()
-        this.filteredDatasets = this.filterData(this.datasets, this.filters)
-        if (this.allAssets) {
-          this.$store.dispatch('DatasetsAssets/get', this.allAssets)
-        }
-      })
-  },
-  mounted () {
-  }
+  watch: {},
+  methods: {}
 }
 </script>
 
 <style lang="stylus" scoped>
-.filters {
-    display: flex;
-    flex-grow: 1;
-    flex-shrink: 0;
-    flex-wrap: nowrap;
-    align-items: flex-end;
-}
+.dataset-page >>> .page__header
+  margin -3.5vw -5vw 0
+  padding 0.5rem 5vw
+  border-bottom 0.5px solid black
+
+p
+  margin 0 !important
+
+.main-content
+  min-width 0
+  max-width 100%
+
+.dashboard-frame
+  width 100%
+  height: 48vw
+  border-radius 8px
+  overflow hidden
+  border 4px solid var(--darkSteel)
+
+.table-of-contents
+  margin-top 1rem
+
+.field-section
+  background-color white
+  transition all .4s ease-out .5s
+
+.field-section h3
+  position relative
+  display inline-block
+
+.field-section h3 a
+  font-size 1rem
 </style>
